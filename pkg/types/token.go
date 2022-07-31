039b98e7e8244b3e5d37b8a87ce228dfba7a9da3
Forbis <37193612+realForbis@users.noreply.github.com>
2022-07-31 19:14:39 +0200

    Create token.go

---
+++ pkg/types/token.go
@@ -0,0 +1,28 @@
+package types
+
+import "math/big"
+
+type TokenInfo struct {
+	TokenId       Hash     `json:"tokenId"`
+	TokenName     string   `json:"tokenName"`
+	TokenSymbol   string   `json:"tokenSymbol"`
+	TotalSupply   *big.Int `json:"totalSupply"`
+	Decimals      uint8    `json:"decimals"`
+	Owner         Address  `json:"owner"`
+	PledgeAmount  *big.Int `json:"pledgeAmount"`
+	WithdrawTime  int64    `json:"withdrawTime"`
+	PledgeAddress Address  `json:"pledgeAddress"`
+	NEP5TxId      string   `json:"NEP5TxId"`
+}
+
+type GenesisTokenInfo struct {
+	TokenId       Hash     `json:"tokenId"`
+	TokenName     string   `json:"tokenName"`
+	TokenSymbol   string   `json:"tokenSymbol"`
+	TotalSupply   *big.Int `json:"totalSupply"`
+	Decimals      uint8    `json:"decimals"`
+	Owner         Address  `json:"owner"`
+	PledgeAmount  *big.Int `json:"pledgeAmount"`
+	WithdrawTime  int64    `json:"withdrawTime"`
+	PledgeAddress Address  `json:"pledgeAddress"`
+}
