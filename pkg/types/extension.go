2b8c8914fc1d79549e972a470883a8a8d96c5a22
Forbis <37193612+realForbis@users.noreply.github.com>
2022-07-31 19:14:09 +0200

    Create extension.go

---
+++ pkg/types/extension.go
@@ -0,0 +1,13 @@
+package types
+
+const (
+	AddressExtensionType   = 99
+	BalanceExtensionType   = 100
+	HashExtensionType      = 101
+	SeedExtensionType      = 102
+	SignatureExtensionType = 103
+	WorkExtensionType      = 104
+	AbiExtensionType       = 105
+	BigNumExtensionType    = 106
+	HexBytesExtensionType  = 107
+)
